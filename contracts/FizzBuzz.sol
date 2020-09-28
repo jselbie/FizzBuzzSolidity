0584128b5358cdba4c65b87c03b0c7ac02ec88f3
John Selbie <jselbie@microsoft.com>
2020-09-27 18:32:51 -0700

    updatd with web conde

--- contracts/FizzBuzz.sol
+++ contracts/FizzBuzz.sol
@@ -9,13 +9,13 @@ contract FizzBuzz {
     event FizzBuzzed(int value, string result);
 
     function fizzbuzz(int value) external returns(string memory) {
+
         string memory result;
 
         bool isDiv2 = ((value % 2) == 0);
         bool isDiv3 = ((value % 3) == 0);
 
-        if (isDiv2 && isDiv3)
-        {
+        if (isDiv2 && isDiv3) {
             result = "FizzBuzz";
         } else if (isDiv2) {
             result = "Fizz";
